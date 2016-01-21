b4b639b787318184768f94e4a682443383319649
gopi <gopi@abc.com>
2016-01-20 17:57:55 -0800

    Initial checkin of sales tool.

---
+++ workspace/SalesTool/src/ca/demo/salestool/Main.java
@@ -0,0 +1,18 @@
+package ca.demo.salestool;
+
+public class Main {
+
+	public static void main(String []args)
+	{
+		SalesData data = new SalesData();
+		
+		displayGreeting();
+		data.display();
+	}
+	
+	private static void displayGreeting()
+	{
+		System.out.println("hello happy sales");
+		System.out.println("this app shows sales data");
+	}
+}
